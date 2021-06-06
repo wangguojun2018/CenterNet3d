c0c79a31a61fc5469f1f9ea6e1e842ada9c650dd
wangguojun2018 <839977837@qq.com>
2021-06-06 16:35:34 +0800

    add sparse_encoder_aux

--- mmdet3d/models/middle_encoders/__init__.py
+++ mmdet3d/models/middle_encoders/__init__.py
@@ -2,4 +2,5 @@ from .pillar_scatter import PointPillarsScatter
 from .sparse_encoder import SparseEncoder
 from .sparse_unet import SparseUNet
 from .sparse_encoderv2 import SparseEncoderV2
-__all__ = ['PointPillarsScatter', 'SparseEncoder', 'SparseUNet','SparseEncoderV2']
+from .sparse_encoder_aux import SparseEncoder_AUX
+__all__ = ['PointPillarsScatter', 'SparseEncoder', 'SparseUNet','SparseEncoderV2','SparseEncoder_AUX']
